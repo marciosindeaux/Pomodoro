7a3f559cb222b815d60a617b693e254ac97e5945
Márcio Alexandre <Freire Sindeaux>
2019-12-29 13:46:27 -0300

    feat: Criação do projeto com VueCLI

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :msg="this.msg"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue App',
+    };
+  },
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style scoped>
+</style>
